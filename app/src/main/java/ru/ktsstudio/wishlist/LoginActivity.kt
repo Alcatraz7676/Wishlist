ec3759e005323308557cf06ef857d8e3cfc2ad71
Maxim Ovchinnikov <maxim.ovchinnikov97@gmail.com>
2019-07-13 14:45:40 +0300

    Fix defects

--- app/src/main/java/ru/ktsstudio/wishlist/LoginActivity.kt
+++ app/src/main/java/ru/ktsstudio/wishlist/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_login.*
+import ru.ktsstudio.wishlist.utils.TextChangedListener
 
 class LoginActivity : AppCompatActivity() {
 
@@ -17,28 +18,12 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun setupEditText() {
-        val textWatcher = object : TextWatcher {
 
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                Log.i("mytag", "${input_login.text.trim().length}; ${input_password.text.trim().length}")
-                if (input_login.text.trim().isNotEmpty() && input_password.text.trim().isNotEmpty()) {
-                    btn_login.isClickable = true
-                    btn_login.alpha = 1f
-                }
-                else {
-                    btn_login.isClickable = false
-                    btn_login.alpha = 0.5f
-                }
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-            }
+        val textChangedListener = TextChangedListener {
+            btn_login.isEnabled = input_login.text.isNotBlank() && input_password.text.isNotBlank()
         }
 
-        input_login.addTextChangedListener(textWatcher)
-        input_password.addTextChangedListener(textWatcher)
+        input_login.addTextChangedListener(textChangedListener)
+        input_password.addTextChangedListener(textChangedListener)
     }
 }
